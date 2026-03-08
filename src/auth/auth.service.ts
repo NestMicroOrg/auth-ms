e29acc310114aefac6d765866fcf8ca9dace9a75
JP19-03 <u202014511@upc.edu.pe>
2026-03-08 18:47:24 -0500

    feat: enhance user registration logic with error handling and return user data

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { prisma } from 'src/prisma.service';
+import { RegisterUserDto } from './dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AuthService {
@@ -7,5 +9,42 @@ export class AuthService {
     private readonly logger = new Logger('AuthService');
 
     constructor() { }
+
+    async registerUser(registerUserDto: RegisterUserDto) {
+
+        const { email, password, name } = registerUserDto;
+
+        try {
+            const user = await prisma.user.findUnique({
+                where: { email }
+            })
+
+            if (user) {
+                throw new RpcException({
+                    statusCode: 400,
+                    message: 'User already exists'
+                })
+            }
+
+            const newUser = await prisma.user.create({
+                data: {
+                    email,
+                    password,
+                    name
+                }
+            })
+
+            return {
+                user: newUser,
+                token: 'abc'
+            }
+
+        } catch (error) {
+            throw new RpcException({
+                statusCode: 400,
+                message: error.message
+            })
+        }
+    }
 
 }
