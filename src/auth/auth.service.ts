0905ad30dd94edf4e6484dfe68c26f46c854be9a
JP19-03 <u202014511@upc.edu.pe>
2026-03-08 19:07:10 -0500

    feat: implement login user functionality with validation and error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { prisma } from 'src/prisma.service';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
 import { RpcException } from '@nestjs/microservices';
 import * as bycript from 'bcrypt'
 
@@ -34,6 +34,46 @@ export class AuthService {
 
             return {
                 user: newUser,
+                token: 'abc'
+            }
+
+        } catch (error) {
+            throw new RpcException({
+                statusCode: 400,
+                message: error.message
+            })
+        }
+    }
+
+    async loginUser(loginUserDto: LoginUserDto) {
+
+        const { email, password } = loginUserDto;
+
+        try {
+            const user = await prisma.user.findUnique({
+                where: { email }
+            })
+
+            if (!user) {
+                throw new RpcException({
+                    statusCode: 400,
+                    message: 'Invalid credentials'
+                })
+            }
+
+            const isPasswordValid = bycript.compareSync(password, user.password);
+
+            if (!isPasswordValid) {
+                throw new RpcException({
+                    statusCode: 400,
+                    message: 'Invalid credentials'
+                })
+            }
+
+            const { password: _, ...result } = user;
+
+            return {
+                user: result,
                 token: 'abc'
             }
 
